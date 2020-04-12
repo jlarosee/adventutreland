9b9d0dd358376630b7a5a38c32f79d23ac704fca
Joshua LaRosee <joshlarosee@gmail.com>
2020-04-12 14:24:35 -0700

    asdf

--- base_v1.js
+++ base_v1.js
@@ -1,14 +1,13 @@
 //
 // Base v1
 //
-
 var attack_mode = true;
 // var target_type = 'slime';
-var mode = 0; //Standing still (will move if target is out of range) = 0, Front of target (Moves to front of target before attacking) = 1, Don't move at all (will not move even if target is out of range) = 2
+var mode = 1; //Standing still (will move if target is out of range) = 0, Front of target (Moves to front of target before attacking) = 1, Don't move at all (will not move even if target is out of range) = 2
 // Movement //
-var mtype = 'armadillo'; //Monster Type of the enemy you want to attack
+var mtype = 'goo'; //Monster Type of the enemy you want to attack
 // Preferred Monster [always keep '' around name] //
-var mtype2 = 'croc'; //Monster Type of the enemy you want to attack if you can't find the first
+var mtype2 = 'goo'; //Monster Type of the enemy you want to attack if you can't find the first
 // Alternate Monster [always keep '' around name] //
 
 var angle = null;
@@ -162,38 +161,38 @@ setInterval(function() {
 var minute_refresh; // how long before the clock resets
 
 function init_xptimer(minref) {
-  minute_refresh = minref || 1;
-  parent.add_log(minute_refresh.toString() + ' min until tracker refresh!', 0x00FFFF);
+    minute_refresh = minref || 1;
+    parent.add_log(minute_refresh.toString() + ' min until tracker refresh!', 0x00FFFF);
 
-  let $ = parent.$;
-  let brc = $('#bottomrightcorner');
+    let $ = parent.$;
+    let brc = $('#bottomrightcorner');
 
-  brc.find('#xptimer').remove();
+    brc.find('#xptimer').remove();
 
-  let xpt_container = $('<div id="xptimer"></div>').css({
-    background: 'black',
-    border: 'solid gray',
-    borderWidth: '5px 5px',
-    width: '320px',
-    height: '96px',
-    fontSize: '28px',
-    color: '#77EE77',
-    textAlign: 'center',
-    display: 'table',
-    overflow: 'hidden',
-    marginBottom: '-5px'
-  });
+    let xpt_container = $('<div id="xptimer"></div>').css({
+        background: 'black',
+        border: 'solid gray',
+        borderWidth: '5px 5px',
+        width: '320px',
+        height: '96px',
+        fontSize: '28px',
+        color: '#77EE77',
+        textAlign: 'center',
+        display: 'table',
+        overflow: 'hidden',
+        marginBottom: '-5px'
+    });
 
-  //vertical centering in css is fun
-  let xptimer = $('<div id="xptimercontent"></div>')
-    .css({
-      display: 'table-cell',
-      verticalAlign: 'middle'
-    })
-    .html('Estimated time until level up:<br><span id="xpcounter" style="font-size: 40px !important; line-height: 28px">Loading...</span><br><span id="xprate">(Kill something!)</span>')
-    .appendTo(xpt_container);
+    //vertical centering in css is fun
+    let xptimer = $('<div id="xptimercontent"></div>')
+        .css({
+          display: 'table-cell',
+          verticalAlign: 'middle'
+        })
+        .html('Estimated time until level up:<br><span id="xpcounter" style="font-size: 40px !important; line-height: 28px">Loading...</span><br><span id="xprate">(Kill something!)</span>')
+        .appendTo(xpt_container);
 
-  brc.children().first().after(xpt_container);
+    brc.children().first().after(xpt_container);
 }
 
 var last_minutes_checked = new Date();
@@ -202,28 +201,31 @@ var last_xp_checked_kill = character.xp;
 // lxc_minutes = xp after {minute_refresh} min has passed, lxc_kill = xp after a kill (the timer updates after each kill)
 
 function update_xptimer() {
-  if (character.xp == last_xp_checked_kill) return;
 
-  let $ = parent.$;
-  let now = new Date();
+    if (character.xp == last_xp_checked_kill) return;
 
-  let time = Math.round((now.getTime() - last_minutes_checked.getTime()) / 1000);
-  if (time < 1) return; // 1s safe delay
-  let xp_rate = Math.round((character.xp - last_xp_checked_minutes) / time);
-  if (time > 60 * minute_refresh) {
-    last_minutes_checked = new Date();
-    last_xp_checked_minutes = character.xp;
-  }
-  last_xp_checked_kill = character.xp;
+    let $ = parent.$;
+    let now = new Date();
+    let time = Math.round((now.getTime() - last_minutes_checked.getTime()) / 1000);
 
-  let xp_missing = parent.G.levels[character.level] - character.xp;
-  let seconds = Math.round(xp_missing / xp_rate);
-  let minutes = Math.round(seconds / 60);
-  let hours = Math.round(minutes / 60);
-  let counter = `${hours}h ${minutes % 60}min`;
+    if (time < 1) return; // 1s safe delay
 
-  $('#xpcounter').text(counter);
-  $('#xprate').text(`${ncomma(xp_rate)} XP/s`);
+    let xp_rate = Math.round((character.xp - last_xp_checked_minutes) / time);
+
+    if (time > 60 * minute_refresh) {
+        last_minutes_checked = new Date();
+        last_xp_checked_minutes = character.xp;
+    }
+    last_xp_checked_kill = character.xp;
+
+    let xp_missing = parent.G.levels[character.level] - character.xp;
+    let seconds = Math.round(xp_missing / xp_rate);
+    let minutes = Math.round(seconds / 60);
+    let hours = Math.round(minutes / 60);
+    let counter = `${hours}h ${minutes % 60}min`;
+
+    $('#xpcounter').text(counter);
+    $('#xprate').text(`${ncomma(xp_rate)} XP/s`);
 }
 
 function ncomma(x) {
@@ -231,3 +233,12 @@ function ncomma(x) {
 }
 
 init_xptimer(5)
+
+/**
+ * Snippets
+ */
+map_key("DOT", {
+    "name":"pure_eval",
+    "code":"ping()",
+    keycode:190
+});
